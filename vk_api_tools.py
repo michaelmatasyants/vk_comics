834466562e16fa4eb59470ce199f952e598c497b
Michael Matasyants <michael.matasyanc@gmail.com>
2023-06-30 19:25:03 +0300

    Remove useless code

--- vk_api_tools.py
+++ vk_api_tools.py
@@ -10,12 +10,6 @@ def check_create_path(to_save_path: Path):
     to_save_path.mkdir(parents=True, exist_ok=True)
 
 
-def get_response(url: str, payload=None) -> requests.models.Response:
-    response = requests.get(url, params=payload)
-    response.raise_for_status()
-    return response
-
-
 def get_filename_extension(image_url: str) -> tuple:
     '''Gets filename and extension from given url'''
     root, extension = os.path.splitext(urlparse(unquote(image_url)).path)
@@ -30,7 +24,8 @@ def save_image(bytes_image: bytes, to_save_path: Path, image_name="image.png"):
 
 
 def download_image(image_url: str, to_save_path: Path, payload=None) -> str:
-    '''Downloads images and places in specified directory or in folder images'''
+    '''Downloads images and places in specified directory or in folder images
+       and returns image name'''
     image_response = requests.get(url=image_url, params=payload)
     image_response.raise_for_status()
     image_name = ''.join(get_filename_extension(image_url=image_url))
